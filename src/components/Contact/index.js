e15a7b3c2d92d277cc6e1f4138211b47bad9bbe4
melaniemdm <melanieborja.pro@gmail.com>
2023-01-25 14:10:34 +0100

    init projet(pluging divers) feature(js & css) contact feature(js & css) landing page delete(js) fonction components

---
+++ src/components/Contact/index.js
@@ -0,0 +1,26 @@
+import './style.scss';
+
+function Contact(){
+    return(
+      <section id="contactMe">  
+        <div className="containerContact" >
+   
+          <div className="containerForm">
+            <h1 className="titleForm">Formulaire de contact</h1>
+   
+            <iframe
+              src="https://tally.so/embed/nrDMOL?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+    
+              frameBorder="0"
+              marginHeight="0"
+              marginWidth="0"
+              title="Nous contacter">
+            </iframe>
+  
+          </div>
+  
+  
+        </div></section>
+    );
+  }
+  export default Contact;
---
+++ src/components/landingPage/index.js
@@ -0,0 +1,51 @@
+import './style.scss';
+import logo from '../../assets/logo.png';
+import video from '../../assets/video.mp4';
+
+export default function Test(){
+
+    return <div className="container">
+    <main className="signup-container">
+      <div className="heading-primary"><img
+          className="logo-image"
+          src={logo}
+          alt="logo"
+        /><span className="span-blue"></span></div>
+      <p className="text-mute">Enter your credentials to access your account.</p>
+   
+  
+      <form className="signup-form">
+        <label className="inp">
+          <input type="email" className="input-text" placeholder="&nbsp;"/>
+          <span className="label">Email</span>
+          <span className="input-icon"><i className="fa-solid fa-envelope"></i></span>
+        </label>
+        <label className="inp">
+          <input type="password" className="input-text" placeholder="&nbsp;" id="password"/>
+          <span className="label">Password</span>
+          <span className="input-icon input-icon-password" data-password><i className="fa-solid fa-eye"></i></span>
+        </label>
+        <button className="btn btn-login">Login</button>
+      </form>
+      <p className="text-mute">Not a member? <a href="$">Sign up</a></p>
+    </main>
+    <div className="welcome-container">
+      <h1 className="heading-secondary">Welcome to  <br></br><span className="lg"> Adaptative Resume !</span>  </h1>
+        <div><p>Créez des CV personnalisés pour
+chaque opportunité et augmentez vos chances de succès. </p>
+<p>Essayez-le dès
+maintenant en participant à notre version beta !</p><br></br></div>
+ <div className="videoContainer">
+				<video src={video} type="video/mp4" controls="true" width="100%" height="100%"/>
+			</div>
+
+      </div>
+
+      
+     
+
+  </div>
+}
+
+
+
