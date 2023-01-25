be53a4465dba0d17cdc966f9bdc04ab7b0a033d5
melaniemdm <melanieborja.pro@gmail.com>
2023-01-25 17:13:36 +0100

    fix(css & js) logo & style

--- src/components/landingPage/index.js
+++ src/components/landingPage/index.js
@@ -1,49 +1,35 @@
 import './style.scss';
 import logo from '../../assets/logo.png';
 import video from '../../assets/video.mp4';
-import { HashLink as Link } from "react-router-hash-link";
+import Contact from '../Contact';
 
 
 export default function LandingPage(){
 
-    return <div className="container">
+    return <div className="containerLanding">
     <main className="signup-container">
       <div className="heading-primary"><img
           className="logo-image"
           src={logo}
           alt="logo"
-        /><span className="span-blue"></span></div>
-      <p className="text-mute">Enter your credentials to access your account.</p>
+        /></div>
+      <p className="text-mute">Essayez-le dès
+maintenant en participant à notre version beta !</p>
    
-  
-      <form className="signup-form">
-        <label className="inp">
-          <input type="email" className="input-text" placeholder="&nbsp;"/>
-          <span className="label">Email</span>
-          <span className="input-icon"><i className="fa-solid fa-envelope"></i></span>
-        </label>
-        <label className="inp">
-          <input type="password" className="input-text" placeholder="&nbsp;" id="password"/>
-          <span className="label">Password</span>
-          <span className="input-icon input-icon-password" data-password><i className="fa-solid fa-eye"></i></span>
-        </label>
-        <button  className="btn btn-login">Login</button>
-      </form>
- 
-      <p>Pour participer à la version beta. Cliquez ici ! <Link to="/demandeContact" className="signup" >   Sign up
-    </Link></p>  
-     
-     
+      <Contact/>
     
     </main>
 
 
     <div className="welcome-container">
       <h1 className="heading-secondary">Welcome to  <br></br><span className="lg"> Adaptative Resume !</span>  </h1>
-        <div className='accroche'><p>Créez des CV personnalisés pour
-chaque opportunité et augmentez vos chances de succès. </p>
-<p>Essayez-le dès
-maintenant en participant à notre version beta !</p><br></br></div>
+        <div className='accroche'>
+          
+          <p><strong>Créez des CV personnalisés</strong> pour chaque opportunité.<br key={1}></br> 
+<p><strong>Augementez vos chances de succès</strong> en proposant des réponses personnalisées à chaque appel d'offres!</p>
+<p>Utilisez notre outils pour <strong>sélectionner les experiences les plus representatives </strong>pour chaque projet. </p>
+</p>
+</div>
  <div className="videoContainer">
 				<video src={video} type="video/mp4" controls={true} width="100%" height="100%"/>
 			</div>
